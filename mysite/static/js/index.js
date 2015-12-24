cd6fb41030f27742b84d944cda2254270bdfb578
Uk-Jin, Yang <ukjin1192@gmail.com>
2015-12-24 16:40:47 +0900

    Add article and comment test page at front-end

--- mysite/static/js/index.js
+++ mysite/static/js/index.js
@@ -8,7 +8,7 @@ var obtainAuthToken = require('./module/obtainAuthToken');
 var refreshAuthToken = require('./module/refreshAuthToken');
 var verifyAuthToken = require('./module/verifyAuthToken');
 
-$(document).on('submit', '#signup-form', function(event) {
+$(document).on('submit', '#create-user-form', function(event) {
   event.preventDefault();
   
   var signupForm = $(this);
@@ -31,9 +31,10 @@ $(document).on('submit', '#signup-form', function(event) {
     contentType: false,
     processData: false
   }).done(function(data) {
+    console.log('Succeed to login');
     obtainAuthToken(loginData);
   }).fail(function(data) {
-    console.log('Login failed');
+    console.log('Failed to login');
     console.log(data);
   }); 
 });
@@ -55,47 +56,35 @@ $(document).on('click', '#logout-btn', function() {
   localStorage.clear();
 });
 
-$(document).on('click', '#user-profile-btn', function() {
+$(document).on('click', '#read-user-btn', function() {
   if (localStorage.getItem('user_id') != null) {
     var userID = localStorage.getItem('user_id');
     
-    setCSRFToken();
     setAuthToken();
     
     $.ajax({
       url: '/api/users/' + userID + '/',
       type: 'GET'
     }).done(function(data) {
+      console.log('Succeed to read user');
       console.log(data);
     }).fail(function(data) {
-      console.log('Get user profile failed');
+      console.log('Failed to read user');
       console.log(data);
     }); 
   }
 });
 
-$(document).on('submit', '#update-user-form', function(event) {
+$(document).on('submit', '#update-user-email-form', function(event) {
   event.preventDefault();
   
   if (localStorage.getItem('user_id') != null) {
     var userID = localStorage.getItem('user_id');
-    var updateUserForm = $(this);
+    var updateUserEmailForm = $(this);
     var formData = new FormData();
     
-    if (updateUserForm.find('[name="email"]').val() != '' && updateUserForm.find('[name="original-password"]').val() != '') {
-      formData.append('email', updateUserForm.find('[name="email"]').val());
-      formData.append('original-password', updateUserForm.find('[name="original-password"]').val());
-    }
-    if (updateUserForm.find('[name="username"]').val() != '') 
-      formData.append('username', updateUserForm.find('[name="username"]').val());
-    if (updateUserForm.find('[name="original-password"]').val() != '' && updateUserForm.find('[name="password"]').val() != '') {
-      formData.append('original-password', updateUserForm.find('[name="original-password"]').val());
-      formData.append('password', updateUserForm.find('[name="password"]').val());
-    }
-    if (updateUserForm.find('[name="deactivate"]').is(':checked') == true && updateUserForm.find('[name="original-password"]').val() != '') { 
-      formData.append('original-password', updateUserForm.find('[name="original-password"]').val());
-      formData.append('is_active', false);
-    }
+    formData.append('email', updateUserEmailForm.find('[name="email"]').val());
+    formData.append('original-password', updateUserEmailForm.find('[name="original-password"]').val());
     
     setCSRFToken();
     setAuthToken();
@@ -107,36 +96,332 @@ $(document).on('submit', '#update-user-form', function(event) {
       contentType: false,
       processData: false
     }).done(function(data) {
-      console.log(data);
-      if (updateUserForm.find('[name="email"]').val() != '' && updateUserForm.find('[name="original-password"]').val() != '') {
+      if (data.state == false) {
+        console.log('Failed to update user email');
+        console.log(data);
+      } else {
+        console.log('Succeed to update user email');
+        console.log(data);
+
         clearAuthToken();
         
         var loginData = new FormData();
         
-        loginData.append('email', updateUserForm.find('[name="email"]').val());
-        loginData.append('password', updateUserForm.find('[name="original-password"]').val());
+        loginData.append('email', updateUserEmailForm.find('[name="email"]').val());
+        loginData.append('password', updateUserEmailForm.find('[name="original-password"]').val());
         
         obtainAuthToken(loginData);
       }
-      if (updateUserForm.find('[name="username"]').val() != '') 
-        localStorage.setItem('username', updateUserForm.find('[name="username"]').val());
-      if (updateUserForm.find('[name="original-password"]').val() != '' && updateUserForm.find('[name="password"]').val() != '') {
+    }).fail(function(data) {
+      console.log('Failed to update user email');
+      console.log(data);
+    }); 
+  }
+});
+
+$(document).on('submit', '#update-user-username-form', function(event) {
+  event.preventDefault();
+  
+  if (localStorage.getItem('user_id') != null) {
+    var userID = localStorage.getItem('user_id');
+    var updateUserUsernameForm = $(this);
+    var formData = new FormData();
+    
+    formData.append('username', updateUserUsernameForm.find('[name="username"]').val());
+    
+    setCSRFToken();
+    setAuthToken();
+    
+    $.ajax({
+      url: '/api/users/' + userID + '/',
+      type: 'PATCH',
+      data: formData,
+      contentType: false,
+      processData: false
+    }).done(function(data) {
+      console.log('Succeed to update user username');
+      console.log(data);
+      localStorage.setItem('username', updateUserUsernameForm.find('[name="username"]').val());
+    }).fail(function(data) {
+      console.log('Failed to update user username');
+      console.log(data);
+    }); 
+  }
+});
+
+$(document).on('submit', '#update-user-password-form', function(event) {
+  event.preventDefault();
+  
+  if (localStorage.getItem('user_id') != null) {
+    var userID = localStorage.getItem('user_id');
+    var updateUserPasswordForm = $(this);
+    var formData = new FormData();
+    
+    formData.append('original-password', updateUserPasswordForm.find('[name="original-password"]').val());
+    formData.append('password', updateUserPasswordForm.find('[name="password"]').val());
+    
+    setCSRFToken();
+    setAuthToken();
+    
+    $.ajax({
+      url: '/api/users/' + userID + '/',
+      type: 'PATCH',
+      data: formData,
+      contentType: false,
+      processData: false
+    }).done(function(data) {
+      if (data.state == false) {
+        console.log('Failed to update user password');
+        console.log(data);
+      } else {
+        console.log('Succeed to update user password');
+        console.log(data);
+
         clearAuthToken();
         
         var loginData = new FormData();
         
         loginData.append('email', localStorage.getItem('email'));
-        loginData.append('password', updateUserForm.find('[name="password"]').val());
+        loginData.append('password', updateUserPasswordForm.find('[name="password"]').val());
         
         obtainAuthToken(loginData);
       }
-      if (updateUserForm.find('[name="deactivate"]').is(':checked') == true && updateUserForm.find('[name="original-password"]').val() != '') 
-        localStorage.clear();
     }).fail(function(data) {
-      console.log('Update user profile failed');
+      console.log('Failed to update user password');
       console.log(data);
     }); 
   }
+});
+
+$(document).on('submit', '#delete-user-form', function(event) {
+  event.preventDefault();
+  
+  if (localStorage.getItem('user_id') != null) {
+    var userID = localStorage.getItem('user_id');
+    var deleteUserForm = $(this);
+    var formData = new FormData();
+    
+    formData.append('original-password', deleteUserForm.find('[name="original-password"]').val());
+    formData.append('is_active', false);
+    
+    setCSRFToken();
+    setAuthToken();
+    
+    $.ajax({
+      url: '/api/users/' + userID + '/',
+      type: 'PATCH',
+      data: formData,
+      contentType: false,
+      processData: false
+    }).done(function(data) {
+      if (data.state == false) {
+        console.log('Failed to delete user');
+        console.log(data);
+      } else {
+        console.log('Succeed to delete user');
+        console.log(data);
+        localStorage.clear();
+      }
+    }).fail(function(data) {
+      console.log('Failed to delete user');
+      console.log(data);
+    }); 
+  }
+});
+
+$(document).on('submit', '#create-article-form', function(event) {
+  event.preventDefault();
+
+  var createArticleForm = $(this);
+  var formData = new FormData();
+
+  formData.append('title', createArticleForm.find('[name="title"]').val());
+  formData.append('context', createArticleForm.find('[name="context"]').val());
+
+  setCSRFToken();
+  setAuthToken();
+
+  $.ajax({
+    url: '/api/articles/',
+    type: 'POST',
+    data: formData,
+    contentType: false,
+    processData: false
+  }).done(function(data) {
+    console.log('Succeed to create article');
+    console.log(data);
+  }).fail(function(data) {
+    console.log('Failed to create article');
+    console.log(data);
+  }); 
+}); 
+
+$(document).on('submit', '#read-article-form', function(event) {
+  event.preventDefault();
+
+  var articleID = $(this).find('[name="article-id"]').val().toString();
+
+  $.ajax({
+    url: '/api/articles/' + articleID + '/',
+    type: 'GET'
+  }).done(function(data) {
+    console.log('Succeed to read article');
+    console.log(data);
+  }).fail(function(data) {
+    console.log('Failed to read article');
+    console.log(data);
+  }); 
+}); 
+
+$(document).on('submit', '#update-article-form', function(event) {
+  event.preventDefault();
+
+  var updateArticleForm = $(this);
+  var formData = new FormData();
+  var articleID = updateArticleForm.find('[name="article-id"]').val().toString();
+
+  formData.append('title', updateArticleForm.find('[name="title"]').val());
+  formData.append('context', updateArticleForm.find('[name="context"]').val());
+
+  setCSRFToken();
+  setAuthToken();
+
+  $.ajax({
+    url: '/api/articles/' + articleID + '/',
+    type: 'PATCH',
+    data: formData,
+    contentType: false,
+    processData: false
+  }).done(function(data) {
+    console.log('Succeed to update article');
+    console.log(data);
+  }).fail(function(data) {
+    console.log('Failed to update article');
+    console.log(data);
+  }); 
+});
+
+$(document).on('submit', '#delete-article-form', function(event) {
+  event.preventDefault();
+
+  var articleID = $(this).find('[name="article-id"]').val().toString();
+  var formData = new FormData();
+
+  formData.append('state', 'deleted');
+
+  setCSRFToken();
+  setAuthToken();
+
+  $.ajax({
+    url: '/api/articles/' + articleID + '/',
+    type: 'PATCH',
+    data: formData,
+    contentType: false,
+    processData: false
+  }).done(function(data) {
+    console.log('Succeed to delete article');
+    console.log(data);
+  }).fail(function(data) {
+    console.log('Failed to delete article');
+    console.log(data);
+  }); 
+});
+
+$(document).on('submit', '#create-comment-form', function(event) {
+  event.preventDefault();
+
+  var createCommentForm = $(this);
+  var formData = new FormData();
+
+  formData.append('article_id', createCommentForm.find('[name="article-id"]').val());
+  formData.append('context', createCommentForm.find('[name="context"]').val());
+
+  setCSRFToken();
+  setAuthToken();
+
+  $.ajax({
+    url: '/api/comments/',
+    type: 'POST',
+    data: formData,
+    contentType: false,
+    processData: false
+  }).done(function(data) {
+    console.log('Succeed to create comment');
+    console.log(data);
+  }).fail(function(data) {
+    console.log('Failed to create comment');
+    console.log(data);
+  }); 
+}); 
+
+$(document).on('submit', '#read-comment-form', function(event) {
+  event.preventDefault();
+
+  var commentID = $(this).find('[name="comment-id"]').val().toString();
+
+  $.ajax({
+    url: '/api/comments/' + commentID + '/',
+    type: 'GET'
+  }).done(function(data) {
+    console.log('Succeed to read comment');
+    console.log(data);
+  }).fail(function(data) {
+    console.log('Failed to read comment');
+    console.log(data);
+  }); 
+}); 
+
+$(document).on('submit', '#update-comment-form', function(event) {
+  event.preventDefault();
+
+  var updateCommentForm = $(this);
+  var formData = new FormData();
+  var commentID = updateCommentForm.find('[name="comment-id"]').val().toString();
+
+  formData.append('context', updateCommentForm.find('[name="context"]').val());
+
+  setCSRFToken();
+  setAuthToken();
+
+  $.ajax({
+    url: '/api/comments/' + commentID + '/',
+    type: 'PATCH',
+    data: formData,
+    contentType: false,
+    processData: false
+  }).done(function(data) {
+    console.log('Succeed to update comment');
+    console.log(data);
+  }).fail(function(data) {
+    console.log('Failed to update comment');
+    console.log(data);
+  }); 
+});
+
+$(document).on('submit', '#delete-comment-form', function(event) {
+  event.preventDefault();
+
+  var commentID = $(this).find('[name="comment-id"]').val().toString();
+  var formData = new FormData();
+
+  formData.append('state', 'deleted');
+
+  setCSRFToken();
+  setAuthToken();
+
+  $.ajax({
+    url: '/api/comments/' + commentID + '/',
+    type: 'PATCH',
+    data: formData,
+    contentType: false,
+    processData: false
+  }).done(function(data) {
+    console.log('Succeed to delete comment');
+    console.log(data);
+  }).fail(function(data) {
+    console.log('Failed to delete comment');
+    console.log(data);
+  }); 
 });
 
 $(document).ready(function() {
